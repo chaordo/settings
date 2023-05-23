bb7502176f040cb8806c84a3cc154696e3d104b0
quintus <john@chaordo.com>
2023-05-23 10:35:26 -0600

    Initial Commit

---
+++ settings/settingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  settingsApp.swift
+//  settings
+//
+//  Created by John on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct settingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
